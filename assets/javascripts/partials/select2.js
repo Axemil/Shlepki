d5a0a9adccc6427cedb0dd483b073083648d473c
Danila Kirillov <cosmowake97@gmail.com>
2020-10-31 14:02:44 +0200

    shlepki

--- assets/javascripts/partials/select2.js
+++ assets/javascripts/partials/select2.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
   $('.select-custom').select2();
 });
+
+$(document).ready(function () {
+  $('.select-custom-city').select2({
+    placeholder: "Выберете город"
+  });
+});
